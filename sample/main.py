f7d31a39fcd6710ebdfafa3fadd6db0754ac83f3
Alex Restrepo <alexrestrepo0826@gmail.com>
2020-10-10 15:24:00 -0500

    mejorando el texto en python

--- sample/main.py
+++ sample/main.py
@@ -1,1 +1,3 @@
 print ("voy a hacer todo lo posible para que luisa Marin sea muy feliz")
+print ("para ello yo me debo convertir en la mejor persona del mundo")
+print ("alguien que este al nivel de tan espectacular persona.")
